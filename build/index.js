e8a39859763fa4f6e973d0731efc4056906faefd
Jordan Onwodi <Jonwodi@hotmail.co.uk>
2022-01-20 15:54:40 +0000

    object types complete

--- build/index.js
+++ build/index.js
@@ -14,3 +14,20 @@ empty = [1, 2, 3];
 let now = new Date();
 let colours = ['red', 'blue', 'green', 10];
 let threeDimesions = [[['one', 'two']]];
+// objects
+let person;
+person = { name: 'Jay', age: 37 };
+let engineer;
+engineer = {
+    firstName: 'Jane',
+    lastName: 'Doe',
+    age: 30,
+    jobTitle: 'Python developer'
+};
+// combing objects rules with declerations
+let developer = {
+    firstName: 'Steven',
+    lastName: 'Smith',
+    age: 30,
+    jobTitle: 'Python developer'
+};
--- src/index.ts
+++ src/index.ts
@@ -19,3 +19,34 @@ let now = new Date();
 let colours = ['red', 'blue', 'green', 10];
 
 let threeDimesions: string[][][] = [[['one', 'two']]];
+
+// objects
+let person: object;
+person = {name: 'Jay', age: 37};
+
+let engineer: {
+  firstName: string,
+  lastName: string,
+  age: number,
+  jobTitle: string
+};
+
+engineer = {
+  firstName: 'Jane',
+  lastName: 'Doe',
+  age: 30,
+  jobTitle: 'Python developer'
+};
+
+// combing objects rules with declerations
+let developer: {
+  firstName: string,
+  lastName: string,
+  age: number,
+  jobTitle: string
+} = {
+  firstName: 'Steven',
+  lastName: 'Smith',
+  age: 30,
+  jobTitle: 'Python developer'
+};
