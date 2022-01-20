0e991d6eb9073b4e5eb599cd2f67cb4ef49f47ac
Jordan Onwodi <Jonwodi@hotmail.co.uk>
2022-01-20 10:38:56 +0000

    first commit

---
+++ build/index.js
@@ -0,0 +1,16 @@
+"use strict";
+let number = 30;
+// The variable greeting will only accept string values or an error will occur.
+let greeting = 'Hello';
+// error occured because greeting only accepts string values not number values.
+// greeting = 9;
+// assigning a variable to null or undefined means that 
+let nothing = null;
+let doesNotExist = undefined;
+let empty = null;
+nothing = 20;
+doesNotExist = 'Goodbye';
+empty = [1, 2, 3];
+let now = new Date();
+let colours = ['red', 'blue', 'green', 10];
+let threeDimesions = [[['one', 'two']]];
---
+++ src/index.ts
@@ -0,0 +1,21 @@
+let number = 30;
+
+// The variable greeting will only accept string values or an error will occur.
+let greeting: string = 'Hello';
+
+// error occured because greeting only accepts string values not number values.
+// greeting = 9;
+
+// assigning a variable to null or undefined means that 
+let nothing = null;
+let doesNotExist = undefined;
+let empty = null;
+nothing = 20;
+doesNotExist = 'Goodbye';
+empty = [1, 2, 3];
+
+let now = new Date();
+
+let colours = ['red', 'blue', 'green', 10];
+
+let threeDimesions: string[][][] = [[['one', 'two']]];
