c9808ad3deb9d28bf84870e3cd951016d965af3a
Jordan Onwodi <Jonwodi@hotmail.co.uk>
2022-01-21 16:52:16 +0000

    function types complete

--- build/index.js
+++ build/index.js
@@ -1,4 +1,6 @@
 "use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const console_1 = require("console");
 let number = 30;
 // The variable greeting will only accept string values or an error will occur.
 let greeting = 'Hello';
@@ -31,3 +33,33 @@ let developer = {
     age: 30,
     jobTitle: 'Python developer'
 };
+// functions
+const SUBTRACT = (a, b) => {
+    return a - b;
+};
+console.log(SUBTRACT(10, 5));
+let minus = SUBTRACT;
+console.log(minus(15, 6));
+function giveName(first, last) {
+    return `${first} ${last}`;
+}
+console.log(giveName('John', 'Smith'));
+// function returns a void (nothing)
+let logger = (message) => {
+    console.log(message);
+};
+// function accepts string but returns any data type
+// functions that accept any data type should be avoided
+let anything = (info) => {
+    return info;
+};
+console.log((0, console_1.info)([1, 2, 3]));
+// functions that accept objects and return objects
+let person1 = (data) => {
+    return data;
+};
+console.log(person1({ name: 'Jay', age: 24, sex: 'male' }));
+let person2 = (data) => {
+    return data;
+};
+console.log(person1({ name: 'Jessica', age: 33, sex: 'female' }));
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,5 @@
+import { info } from "console";
+
 let number = 30;
 
 // The variable greeting will only accept string values or an error will occur.
@@ -50,3 +52,40 @@ let developer: {
   age: 30,
   jobTitle: 'Python developer'
 };
+
+// functions
+const SUBTRACT = (a: number, b: number): number => {
+  return a - b;
+}
+console.log(SUBTRACT(10, 5));
+let minus = SUBTRACT;
+console.log(minus(15, 6));
+
+function giveName(first: string, last: string) {
+  return `${first} ${last}`;
+}
+console.log(giveName('John', 'Smith'));
+
+// function returns a void (nothing)
+let logger = (message: string): void => {
+  console.log(message);
+}
+
+// function accepts string but returns any data type
+// functions that accept any data type should be avoided
+let anything = (info: string): any => {
+  return info;
+}
+console.log(info([1,2,3]));
+
+// functions that accept objects and return objects
+let person1 = (data: object): object => {
+  return data;
+}
+console.log(person1({name: 'Jay', age: 24, sex: 'male'}));
+
+let person2 = (data: {}): {} => {
+  return data;
+}
+console.log(person1({name: 'Jessica', age: 33, sex: 'female'}));
+
